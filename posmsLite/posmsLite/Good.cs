287b46ff3b7309edac71bd045e7a4bb359485afb
Andrey Shcherbak <andrey.sher2015@gmail.com>
2019-11-09 23:16:32 +0200

    Add shop goods

--- posmsLite/posmsLite/Good.cs
+++ posmsLite/posmsLite/Good.cs
@@ -21,16 +21,16 @@ namespace posmsLite
         public GoodCategory Category { get; set; }
         private static string[] randName = { "Bread","Butter","Egg", "Water", "Sugar", "Salt", "Tea","Meat", "Chease", "Instant noodles", "Cake", "Toilet paper", "Soap", "Fish" };
 
-        private static string[] randMilk = { "Milk"}; 
-        private static string[] randBakery = { "Bread"};
-        private static string[] randHouseholdСhemicals = {"Domestos" };
-        private static string[] randMeat = {"Pork" };
-        private static string[] randFish = {"Salmon" };
-        private static string[] randCereals = {"Cereals" };
+        private static string[] randMilk = { "Milk", "Chease","Kefir", "Yogurt" }; 
+        private static string[] randBakery = { "Bread", "Scone","Pita" };
+        private static string[] randHouseholdСhemicals = { "Shower gel", "Powder", "Shampoo", "Pipe cleaner", "Soap" };
+        private static string[] randMeat = {"Pork", "Beef", "Chicken", "Lamb" };
+        private static string[] randFish = {"Salmon" , "Herring", "Hake", "Sturgeon", "Trout" };
+        private static string[] randCereals = {"Cereals", "Oatmeal", "Buckwheat", "Perlovka", "Rice" };
         private static string[] randPasta = {"Instant noodles" };
-        private static string[] randFruit = {"Apple" };
-        private static string[] randVegetables = {"Tomato" };
-        private static string[] randGrocery = { "Salt" };
+        private static string[] randFruit = {"Apple", "Avocado", "Pineapple", "Orange", "Banana" };
+        private static string[] randVegetables = {"Tomato", "Eggplant", "Broccoli", "Potato", "Corn", "Carrot" };
+        private static string[] randGrocery = { "Salt", "Sugar", "Tea", "Cake" };
 
 
         public Good()
